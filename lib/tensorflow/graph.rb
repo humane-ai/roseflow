10535a01f132f3067504ccb28df0d773fdc470e5
Lauri Jutila <ruby@laurijutila.com>
2017-03-26 00:25:35 +0200

    Adds JSON graph fixture. Adds methods to load graph definition from file or JSON.

--- lib/tensorflow/graph.rb
+++ lib/tensorflow/graph.rb
@@ -1,6 +1,17 @@
+require "tensorflow/libtensorflow"
+require "tensorflow/libtensorflow/protobuf"
+
 module TensorFlow
+  module Errors
+    class JSONValidationError < StandardError
+    end
+  end
+
   class Graph
+    PROTOBUF_CLASS = ::TensorFlow::LibTensorFlow::Protobuf::GraphDef
     attr_reader :name
+    attr_reader :nodes
+    attr_reader :definition
 
     def initialize(*args)
       if args && args.is_a?(Hash) && args[:name]
@@ -8,14 +19,25 @@ module TensorFlow
       else
         @name = "My Graph"
       end
+      @nodes = []
     end
 
     def name=(value)
       @name = value
     end
 
+    def from_json(json)
+      @definition = ::Google::Protobuf.decode_json(PROTOBUF_CLASS, json)
+    end
+
+    def from_file(file)
+      @definition = ::Google::Protobuf.decode(PROTOBUF_CLASS, file.read)
+    ensure
+      file.close
+    end
+
     def to_graphdef
-      TensorFlow::LibTensorFlow::Protobuf::GraphDef.new
+      PROTOBUF_CLASS.new
     end
   end
 end
--- spec/tensorflow/graph_spec.rb
+++ spec/tensorflow/graph_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe TensorFlow::Graph do
   describe "Attributes" do
     subject(:subject) { described_class.new }
 
-    describe "name" do
+    describe "#name" do
       it "has a name" do
         expect(subject.name).to eq "My Graph"
       end
@@ -18,6 +18,91 @@ RSpec.describe TensorFlow::Graph do
       it "can have a custom name" do
         subject.name = "Awesome Graph"
         expect(subject.name).to eq "Awesome Graph"
+      end
+    end
+
+    describe "#description" do
+
+    end
+
+    describe "#definition" do
+
+    end
+
+    describe "#nodes" do
+      it "returns the nodes of the graph" do
+        expect(subject.nodes).to be_a Array
+        expect(subject.nodes).to be_empty
+      end
+    end
+  end
+
+  describe "Inputs" do
+
+  end
+
+  describe "Outputs" do
+
+  end
+
+  describe "Loading graph definitions" do
+    context "from a protobuf file" do
+      context "- Successful" do
+        let(:file) { File.open(fixture_path + "/graph/regression_simplified.pb", "r") }
+        let(:graph) { described_class.new }
+
+        subject(:subject) { graph.from_file(file) }
+
+        it "loads graph definition from protobuf file" do
+          expect(subject).to be_truthy
+          expect(graph.definition).to be_a described_class::PROTOBUF_CLASS
+          expect(graph.definition.node.count).to eq 13
+          expect(graph.definition.node.first.name).to eq "Variable/initial_value"
+        end
+      end
+
+      context "- Failure" do
+        let(:file) { File.open(fixture_path + "/graph/graph_multi_dim.pbtxt", "r") }
+        let(:graph) { described_class.new }
+
+        subject(:subject) { graph.from_file(file) }
+
+        it "fails to load files that are not protobuf binaries" do
+          expect{ subject }.to raise_error(Google::Protobuf::ParseError).with_message(/Invalid wire type/)
+        end
+      end
+    end
+
+    context "from JSON" do
+      context "- Successful" do
+        let(:json) { File.read(fixture_path + "/graph/json/regression.json") }
+        let(:graph) { described_class.new }
+
+        subject(:subject) { graph.from_json(json) }
+
+        it "loads graph definition from JSON" do
+          expect(subject).to be_truthy
+          expect(graph.definition).to be_a described_class::PROTOBUF_CLASS
+          expect(graph.definition.node.count).to eq 13
+          expect(graph.definition.node.first.name).to eq "Variable/initial_value"
+        end
+      end
+
+      context "- Failure" do
+        let(:string) { "Pure string" }
+        let(:json) { { "foo": "bar" } }
+        let(:graph) { described_class.new }
+
+        subject(:string_action) { graph.from_json(string) }
+        subject(:json_action) { graph.from_json(json) }
+
+        it "fails with appropriate error when input is not JSON" do
+          expect{ string_action }.to raise_error(Google::Protobuf::ParseError)
+        end
+
+        it "fails with JSONValidationError when JSON does not contain necessary elements", skip: "Requires implementation of JSON validator" do
+          expect{ json_action }.to raise_error(TensorFlow::Errors::JSONValidationError)
+        end
       end
     end
   end
@@ -30,7 +115,7 @@ RSpec.describe TensorFlow::Graph do
         subject(:subject) { graph.to_graphdef }
 
         it "returns GraphDef of the graph" do
-          expect(subject).to be_a TensorFlow::LibTensorFlow::Protobuf::GraphDef
+          expect(subject).to be_a described_class::PROTOBUF_CLASS
           expect(subject.node).to be_empty
           expect(subject.library).to be_nil
           expect(subject.version).to eq 0
@@ -43,7 +128,7 @@ RSpec.describe TensorFlow::Graph do
       end
 
       context "Graph with versions" do
-        
+
       end
     end
   end
